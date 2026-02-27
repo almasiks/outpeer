8ea69009bfebf676c8c384c8cefd5e1dce1cfb5e
Almas Magrupov <magrupovalmas06@gmail.com>
2026-02-28 00:56:00 +0500

    added author page

--- articles_app/articlehub/articles/urls.py
+++ articles_app/articlehub/articles/urls.py
@@ -2,4 +2,6 @@ from django.urls import path
 from . import views
 urlpatterns = [
     path('', views.articles_list, name='articles_list'),
+    path('<int:article_id>/', views.article_detail, name='article_detail'),
+    path('author/<int:author_id>/', views.author_detail, name='author_detail'),
 ]
--- articles_app/articlehub/articles/views.py
+++ articles_app/articlehub/articles/views.py
@@ -3,3 +3,11 @@ from django.http import HttpResponse
 from django.http import HttpResponseRedirect
 def articles_list(request):
     return render(request, 'articles/article_list.html')
+
+
+def article_detail(request, article_id):
+    return HttpResponse(f'Это страница статьи с id: {article_id}')
+
+
+def author_detail(request, author_id):
+    return HttpResponse(f'Это страница автора с id: {author_id}')
