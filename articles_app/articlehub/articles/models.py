4ba06835f2ac12a9e731ca0cd43271ccb180208a
Almas Magrupov <magrupovalmas06@gmail.com>
2026-03-12 20:18:03 +0500

    Работа с запросами и навигацией в Django

--- articles_app/articlehub/articles/models.py
+++ articles_app/articlehub/articles/models.py
@@ -1,3 +1,22 @@
+from tkinter.constants import CASCADE
+
 from django.db import models
 
-# Create your models here.
+class Author(models.Model):
+    name = models.CharField(max_length=100)
+    email = models.EmailField(unique=True)
+
+    def __str__(self):
+        return self.name
+class Article(models.Model):
+    title = models.CharField(max_length=200)
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    isPublished = models.BooleanField(default=False)
+    author = models.ForeignKey(Author, on_delete= models.CASCADE, related_name='articles' )
+
+
+
+
+    def __str__(self):
+        return self.title
--- minisocial_project/accounts/models.py
+++ minisocial_project/accounts/models.py
@@ -1,3 +1,23 @@
-from django.db import models
+from tkinter.constants import CASCADE
 
-# Create your models here.
+from django.db import models
+from django.contrib.auth.models import User
+
+class Post(models.Model):
+    title = models.CharField(max_length=100)
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='posts')
+
+def __str__(self):
+    return f"{self.title}"
+
+
+class Comment(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='comments')
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='accounts_comments')
+    text = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"comments by: {self.author.username} on {self.post.title}"
