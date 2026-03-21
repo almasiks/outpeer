941f1428410dde6e652bee653d2bccb7e35e42b1
Almas Magrupov <magrupovalmas06@gmail.com>
2026-03-21 22:35:38 +0500

    Шаблонизатор

--- minisocial_project/posts/urls.py
+++ minisocial_project/posts/urls.py
@@ -4,6 +4,7 @@ from . import views
 urlpatterns = [
     path('', views.post_list, name='post_list'),
     path('user/<int:user_id>/posts/', views.user_posts, name='user_posts'),
+    path('<int:post_id>/', views.post_detail, name='post_detail'),
     path('posts/create/', views.create_post, name='create_post'),
     path('posts/<int:post_id>/edit/', views.edit_post, name='edit_post'),
     path('posts/<int:post_id>/delete/', views.delete_post, name='delete_post'),
